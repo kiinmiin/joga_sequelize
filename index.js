4a634ce094b53d64531f3e9b0353f4e4beaefc24
kiinmiin <rasmo.kiin@voco.ee>
2025-02-05 18:52:52 +0200

    Created connection to database

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize');
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connected to the database');
+    })
+    .catch(err => {
+        console.error('Error connecting to the database:', err);
+    });
+
 app.get("/", (req, res) => {
     res.json({message: "Welcome to sequelize application"});
 });
